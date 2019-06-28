7485d096781d10b3958ed31d78a24f46a01a5e20
Student <example@example.com>
2019-06-28 08:57:53 -0700

    add readme with specs

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # Bagheera's Pizzeria
 
-#### _A web site for ordering pizza - June 21, 2019_
+#### _Determines Age and things for Different planets - June 28, 2019_
 
-#### _By **Na Hyung Choi**_
+#### _Emerson Jordan**_
 
 ## Description
 
-On this web site, the user can choose toppings, crust, and a size for one or more pizzas, see the details of the order and the final cost, and "place an order."
+#### _A user will input their date of birth and the program will return  information about their age and days left on each planet_
 
 ### Specs
 | Spec | Input | Output |
-| :-------------     | :------------- | :------------- |
-| **Creates a new pizza through a constructor** | var pizza1 = new Pizza ("large", ["cheese", "sausage"]) | pizza1 = { size : "large", toppings: ["cheese", "sausage"], cost: undefined} |
+| -------------     | ------------- | ------------- |
+| **Converts date of birth to number of days lived** | august 15, 1950 | 25154 |
+|**Converts days lived to Mercury years** | 180 | 2 |
+|**Converts days lived to Venus years** | 810 | 3 |
+|**Converts days lived to Mars years** | 4129 | 6 |
+|**Converts days lived to Jupiter years** | 50032 | 11 |
+|**Gives years Left user has left on each planet, or years past life expectancy if already passed** | Birth date | days left on: mercury, Venus, Mars, Jupiter |
+
 
 ## Setup/Installation Requirements
 
 * Clone this repository and open the .html file.
-* OR go directly to the [Web page](http://schoinh.github.io/pizza)
+* Download NPM from https://www.npmjs.com/get-npm
+* Navigate to this file in the terminal and type "npm install" and press enter
+* Enter "run start" in the terminal
 
 ## Known Bugs
 * No known bugs at this time.
@@ -25,13 +33,14 @@ On this web site, the user can choose toppings, crust, and a size for one or mor
 * JavaScript
 * jQuery
 * Bootstrap
+* Node Package Manager
 
 ## Support and contact details
 
-_Please contact Na Hyung with questions and comments._
+_Please contact Emerson Jordan with questions and comments._
 
 ### License
 
 *GNU GPLv3*
 
-Copyright (c) 2019 **_Na Hyung Choi_**
+Copyright (c) 2019 **_Emerson Jordan_**
--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,97 +1,11 @@
-import { syllablesInWord, countVowels, countDiphthongs, countSilentVowels, isHaiku} from './../src/haikuChecker-code';
-
-
-describe ('SyllableTest', function(){
-
-  // it('should return syllables in word', function(){
-  //   expect(syllablesInWord('yellow')).toEqual(2);
-  // });
-  //you
-  // // it('should return syllables in word the second', function(){
-  // //   expect(syllablesInWord('word')).toEqual(2);
-  // // })
-  //
-  // it('should return # of vowels in a word', function(){
-  //   expect(countVowels("Trevor")).toEqual(2);
-  // });
-  //
-  // it('should return # of vowels in a word2', function(){
-  //   expect(countVowels("TrEvor")).toEqual(2);
-  // });
-  // // it('should return the number of diphthongs', function(){
-  // //   expect(countDiphthongs("house")).toEqual(1);
-  // // })
-  // //
-  // // it('should return the number of diphthongs', function(){
-  // //   expect(countDiphthongs("roundhouse")).toEqual(2);
-  // // })
-  // it('should return the number of diphthongs', function(){
-  //   expect(countDiphthongs("boat")).toEqual(1);
-  // });
-  //
-  // it('should return the number of diphthongs', function(){
-  //   expect(countDiphthongs("year")).toEqual(1);
-  // });
-  //
-  // it('should return the number of silent vowels1', function(){
-  //   expect(countSilentVowels("house")).toEqual(1);
-  // });
-  //
-  // it('should return the number of silent vowels2', function(){
-  //   expect(countSilentVowels("neat")).toEqual(0);
-  // });
-  //
-  // it('should return the number of silent vowels3', function(){
-  //   expect(countSilentVowels("le")).toEqual(0);
-  // });
-  // it('return syllables in a word 1', function(){
-  //   expect(syllablesInWord("marquee")).toEqual(2);
-  // });
-  // it('return syllables in a word 2', function(){
-  //   expect(syllablesInWord("peanut")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 3', function(){
-  //   expect(syllablesInWord("Trever")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 4', function(){
-  //   expect(syllablesInWord("Emerson")).toEqual(3);
-  // });
-  //
-  // it('return syllables in a word 5', function(){
-  //   expect(syllablesInWord("roundhouse")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 6', function(){
-  //   expect(syllablesInWord("blackguard")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 7', function(){
-  //   expect(syllablesInWord("tickle")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 8', function(){
-  //   expect(syllablesInWord("outside")).toEqual(2);
-  // });
-  //
-  // it('return syllables in a word 9', function(){
-  //   expect(syllablesInWord("tyranosaurus")).toEqual(5);
-  // });
-  // it('return syllables in a word 10', function(){
-  //   expect(syllablesInWord("tree")).toEqual(1);
-  // });
-  // it('return syllables in a word 11', function(){
-  //   expect(syllablesInWord("shoe")).toEqual(1);
-  // });
-
-  it('should return true/false if legal Haiku', function(){
-    let line1 = "counting syllables"
-    let line2 = "is harder than you might think"
-    let line3 = "coyote marquee"
-
-    expect(isHaiku(line1,line2,line3)).toEqual(true)
-
-  })
-
-});
+// import { syllablesInWord} from './../src/haikuChecker-code';
+//
+//
+// describe ('SyllableTest', function(){
+//
+//    it('return syllables in a word 11', function(){
+//      expect(syllablesInWord("shoe")).toEqual(1);
+//    });
+//
+//
+// });
